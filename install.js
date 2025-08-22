842a524392d31ca654ab6d856ca096d45a46d9b2
Drew Daniels <drew.daniels@webpt.com>
2025-08-22 13:30:27 -0500

    cleanup

--- install.js
+++ install.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
+'use strict';
+
+// inspiration: https://github.com/facebook/create-react-app/blob/main/tasks/cra.js
+
+const fs = require('fs');
+const path = require('path');
+const cp = require('child_process');
+
+
+const handleExit = () => {
+    cleanup();
+    console.log('Exiting without error.');
+    process.exit();
+};
+
+const handleError = e => {
+    console.error('ERROR! An error was encountered while executing');
+    console.error(e);
+    cleanup();
+    console.log('Exiting with error.');
+    process.exit(1);
+};
+
+process.on('SIGINT', handleExit);
+process.on('uncaughtException', handleError);
+
 console.log('Hi!')
 
 // Verify project name passed as an argument
@@ -21,3 +47,13 @@ console.log('Hi!')
 // Create cspell.config.js
 
 // Create cucumber.js
+
+// Initialize folder structure
+const gitStatus = cp.execSync(`git status --porcelain`).toString();
+
+fs.mkDir()
+
+mkdir src
+mki
+
+// Initialize git repository
