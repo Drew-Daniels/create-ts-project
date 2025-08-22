6b8ff1cee3ad12002c42b6d558b90562b617e260
Drew Daniels <drew.daniels@webpt.com>
2025-08-22 13:25:13 -0500

    outline of steps

--- install.js
+++ install.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
 console.log('Hi!')
+
+// Verify project name passed as an argument
+
+// Create new directory in ~/projects/<project-name>
+
+// Create .mise.toml
+
+// Create initial package.json
+
+// Install developer dependencies
+
+// Install runtime dependencies
+
+// Create tsconfig.json
+
+// Create eslint.config.js
+
+// Create cspell.config.js
+
+// Create cucumber.js
