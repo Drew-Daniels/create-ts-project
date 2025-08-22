61217ba12853d41378ed92c16569dc418b908318
Drew Daniels <drew.daniels@webpt.com>
2025-08-22 13:38:36 -0500

    refactor to ts

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'use strict';
+import cp from 'child_process';
+const cleanup = () => {
+    console.log('Cleaning up.');
+    // Reset changes made to package.json files.
+    cp.execSync(`git checkout -- packages/*/package.json`);
+    // Uncomment when snapshot testing is enabled by default:
+    // rm ./template/src/__snapshots__/App.test.js.snap
+};
+const handleExit = () => {
+    cleanup();
+    console.log('Exiting without error.');
+    process.exit();
+};
+const handleError = (e) => {
+    console.error('ERROR! An error was encountered while executing');
+    console.error(e);
+    cleanup();
+    console.log('Exiting with error.');
+    process.exit(1);
+};
+process.on('SIGINT', handleExit);
+process.on('uncaughtException', handleError);
+console.log('Hi!');
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// inspiration: https://github.com/facebook/create-react-app/blob/main/tasks/cra.js
+
+import fs from 'fs';
+import path from 'path';
+import cp from 'child_process';
+
+const cleanup = () => {
+  console.log('Cleaning up.');
+  // Reset changes made to package.json files.
+  cp.execSync(`git checkout -- packages/*/package.json`);
+  // Uncomment when snapshot testing is enabled by default:
+  // rm ./template/src/__snapshots__/App.test.js.snap
+};
+
+const handleExit = () => {
+  cleanup();
+  console.log('Exiting without error.');
+  process.exit();
+};
+
+const handleError = (e: Error) => {
+  console.error('ERROR! An error was encountered while executing');
+  console.error(e);
+  cleanup();
+  console.log('Exiting with error.');
+  process.exit(1);
+};
+
+process.on('SIGINT', handleExit);
+process.on('uncaughtException', handleError);
+
+console.log('Hi!')
+
+// Verify project name passed as an argument
+
+// Create new directory in ~/projects/<project-name>
+
+// Create mise.toml
+
+// Create initial package.json
+
+// Install developer dependencies
+
+// Install runtime dependencies
+
+// Create tsconfig.json
+
+// Create eslint.config.js
+
+// Create cspell.config.js
+
+// Create cucumber.js
+
+// Initialize folder structure
+
+// Initialize git repository
